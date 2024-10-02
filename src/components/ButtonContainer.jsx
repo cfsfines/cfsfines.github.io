c50f0bdf34c9ab6d2935e73a8a3629ffe35039b2
cfsfines <cfsfines99@gmail.com>
2024-10-01 22:32:22 -0700

    reimplemented entire website in react

---
+++ src/components/ButtonContainer.jsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+import StyledButton from "./StyledButton";
+
+const StyledButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 75%;
+  flex-wrap: wrap;
+  gap: 15px;
+`;
+
+
+const ButtonContainer = ({ updateCurrProj, buttons }) => {
+  return (
+    <>
+      <StyledButtonContainer>
+        {buttons.map((element, idx) => (
+          <StyledButton
+            key={element.Title}
+            onClick={() => {
+              updateCurrProj(buttons[idx]);
+            }}
+          >
+            {element.Title}
+          </StyledButton>
+        ))}
+      </StyledButtonContainer>
+    </>
+  );
+};
+
+export default ButtonContainer;
